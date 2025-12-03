4f03062a757dd2119b34dc71fd106079e90dc47c
Shreyansh Gupta <hardikshrey123@gmail.com>
2025-12-03 23:39:07 +0530

    Add button with press counter to index screen, hide navigation header

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,39 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const [pressCount, setPressCount] = useState(0);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Pressable 
+        onPress={() => setPressCount(pressCount + 1)} 
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Print animation</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    width: "100%",
+    maxWidth: 140,
+    height: 32,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 9999,
+    backgroundColor: "#FFF",
+  },
+  buttonText: {
+    fontSize: 14,
+    fontWeight: "500",
+    color: "black",
+  },
+});
