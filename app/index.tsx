a104bafd4e5f6d2227f3a9dcfd5f06b34d7e50ea
Shreyansh Gupta <hardikshrey123@gmail.com>
2025-12-03 23:53:33 +0530

    Add bottom sheet component with gesture handler support

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,37 @@
-import { useState } from "react";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import { useCallback, useMemo, useRef } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
-  const [pressCount, setPressCount] = useState(0);
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  const snapPoints = useMemo(() => ["90%"], []);
+
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log("handleSheetChanges", index);
+  }, []);
+
+  const handleSnapPress = useCallback(() => {
+    bottomSheetRef.current?.snapToIndex(1);
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Pressable 
-        onPress={() => setPressCount(pressCount + 1)} 
-        style={styles.button}
-      >
+      <Pressable onPress={handleSnapPress} style={styles.button}>
         <Text style={styles.buttonText}>Print animation</Text>
       </Pressable>
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        index={-1}
+        snapPoints={snapPoints}
+        onChange={handleSheetChanges}
+        enablePanDownToClose={true}
+      >
+        <BottomSheetView style={styles.contentContainer}>
+          <Text>Awesome Bottom Sheet 🎉</Text>
+        </BottomSheetView>
+      </BottomSheet>
     </View>
   );
 }
@@ -21,6 +41,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "grey",
   },
   button: {
     width: "100%",
@@ -35,5 +56,10 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: "500",
     color: "black",
+  },
+  contentContainer: {
+    flex: 1,
+    padding: 36,
+    alignItems: "center",
   },
 });
